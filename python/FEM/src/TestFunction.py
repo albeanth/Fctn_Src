c98c4863f68d2ee5a6a9dc4f68ab678ae63d12e4
albeanth <albertia@oregonstate.edu>
2019-11-03 11:19:58 -0800

    Updating TestFunction: 1) Adding several more MMS tests. Including piecewise tests that showcase DFEM abilities. 2) Updating heterogeneity flag to be simple true/false for ease of use.

--- python/FEM/src/TestFunction.py
+++ python/FEM/src/TestFunction.py
@@ -20,27 +20,48 @@ class func:
         """
         print selection to screen
         """
-        if self.selection == 1:
-            print("\nReflecting BCs ", end="")
+        if self.selection == 0:
+            print("\nLinear Solution test, Dirichlet BCs left and right, ", end="")
+        elif self.selection == 1:
+            print("\nQuadratic Solution test, Dirichlet BCs left and right, ", end="")
         elif self.selection == 2:
-            print("\nDirichlet left, reflecting right ",end="")
+            print("\nCubic Solution test, Dirichlet BCs left and right, ", end="")
+        elif self.selection == 3:
+            print("\nQuintic Solution test, Dirichlet BCs left and right, ", end="")
         elif self.selection == 4:
+            print("\nCosine Solution test, Reflecting BCs ", end="")
+        elif self.selection == 5:
+            print("\nPiecewise Solution test, Dirichlet BCs left and right, ", end="")
+        elif self.selection == 6:
+            print("\nDirichlet left, reflecting right ",end="")
+        elif self.selection == 7:
             print("\nNumerical ringing test ",end="\n")
-        if self.hetero == 1:
+        if self.hetero == False:
             print("with homogeneous cross sections.",end="\n\n")
-        elif self.hetero == 2:
+        elif self.hetero == True:
             print("with smooth heterogeneous cross sections.",end="\n\n")
 
     def u(self, x):
         """
         base function definition
         """
-        if self.selection == 1:
-            val = cos(x)+1.0
+        if self.selection == 0:
+            val = 1.0 - x
+        elif self.selection == 1:
+            val = x**2 - x + 1.0
         elif self.selection == 2:
-            val = cos(x-pi/2)+1.0
+            val = (x-1)**3 + 1.0
         elif self.selection == 3:
-            val = 1/(1+x**2)
+            val = x**5 - 5.0*x**4 + 5*x**3 + 5*x**2 - 6*x + 5
+        elif self.selection == 4:
+            val = cos(x)+1.0
+        elif self.selection == 5:
+            if x < 1:
+                val = x**2 + 1.0 # x + 1.0
+            else:
+                val = -(x-1.0)**3 + 1.0 # -x + 2.0
+        elif self.selection == 6:
+            val = cos(x-pi/2)+1.0
         else:
             print("def u(self,x): unknown problem selection!")
             exit(-1)
@@ -50,12 +71,23 @@ class func:
         """
         derivative of base function
         """
-        if self.selection == 1:
-            val = -sin(x)
+        if self.selection == 0:
+            val = -1.0
+        elif self.selection == 1:
+            val = 2*x - 1.0
         elif self.selection == 2:
-            val = cos(x)
+            val = 3*(x-1.0)**2
         elif self.selection == 3:
-            val = (-2*x)/((1+x**2)**2)
+            val = 5*x**4 - 20*x**3 + 15*x**2 + 10*x - 6
+        elif self.selection == 4:
+            val = -sin(x)
+        elif self.selection == 5:
+            if x < 1.0:
+                val = 2.0*x #1.0
+            else:
+                val = -3.0*(x-1.0)**2 #-1.0
+        elif self.selection == 6:
+            val = cos(x)
         else:
             print("def up(self, x): unknown problem selection!")
             exit(-1)
@@ -65,12 +97,23 @@ class func:
         """
         derivative of base function
         """
-        if self.selection == 1:
-            val = -cos(x)
+        if self.selection == 0:
+            val = 0.0
+        elif self.selection == 1:
+            val = 2.0
         elif self.selection == 2:
-            val = -sin(x)
+            val = 6*(x-1.0)
         elif self.selection == 3:
-            val = 8*x**2/(1+x**2)**3 - 2/(1+x**2)**2
+            val = 20*x**3 - 60*x**2 + 30*x + 10
+        elif self.selection == 4:
+            val = -cos(x)
+        elif self.selection == 5:
+            if x < 1.0:
+                val = 2.0  # 0.0
+            else:
+                val = -6*(x-1.0)  # 0.0
+        elif self.selection == 6:
+            val = -sin(x)
         else:
             print("def upp(self, x): unknown problem selection!")
             exit(-1)
@@ -80,17 +123,23 @@ class func:
         """
         absorption cross section
         """
-        if self.hetero == 1:
+        if self.hetero == False:
             val = 1.0 # homogeneous case
-        elif self.hetero == 2:
-            val = x+1.0  # heterogeneous case
-        elif self.selection == 4:
-            if x < 2:
-                val = 1.0E-4
-            elif x < 4:
-                val = 100.0
+        elif self.hetero == True:
+            val = x+1.0
+        
+        if self.selection == 5:
+            if x < 1.0:
+                val = 1.0E4
             else:
-                val = 100.0
+                val = 1.0E4
+        # elif self.selection == 7:
+        #     if x < 2:
+        #         val = 1.0E10
+        #     elif x < 4:
+        #         val = 1.0E10
+        #     else:
+        #         val = 1.0E10
         else:
             print("def SigA(self, x): unknown heterogeneity selection!")
             exit(-1)
@@ -100,14 +149,19 @@ class func:
         """
         diffusion coefficient, D(x)
         """
-        val = 1.0/(3.0*self.SigA(x)) # heterogeneous case
+        val = 1.0/(3.0*self.SigA(x))
+        if self.selection == 5:
+            if x < 1.0:
+                val = 1.0E-4
+            else:
+                val = 1.0E-2
         return val
 
     def Dx(self, x):
         """
         first spatial derivative of diffusion coefficient, D'(x)
         """
-        if self.hetero == 1:
+        if self.hetero == False:
             val = 0.0    # homogeneous case
         else:
             val = -1.0/(3*(x+1)**2.0)  # heterogeneous case
@@ -117,15 +171,15 @@ class func:
         """
         source using T and Tp that defines the MMS problem
         """
-        if ((self.selection == 1) or (self.selection == 2) or (self.selection == 3)):
+        if self.selection < 7:
             val = -(self.Dx(x)*self.up(x) + self.D(x)*self.upp(x)) + self.SigA(x)*self.u(x)
-        elif self.selection == 4:
+        elif self.selection == 7:
             if x < 2:
-                val = 100.0
+                val = 1.0E-1
             elif x < 4:
-                val = 2.0
+                val = 1.0E-1
             else:
-                val = 2.0
+                val = 1.0E-1
         else:
             print("def f(self, x): unknown problem selection!")
             exit(-1)
