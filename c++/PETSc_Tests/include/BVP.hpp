0383f004bba578d526a6937ab38ae4de9411029c
albeanth <albertia@oregonstate.edu>
2019-12-03 17:21:43 -0800

    Add petsc functionality for eval'ing basis funcs - moving petsc Mat and Vec declarations to header files - creating specific petsc based functions to evaluate basis functions and creating vectors to store them - need to create petsc fucntion to complete outer product and build local matrices

--- c++/PETSc_Tests/include/BVP.hpp
+++ c++/PETSc_Tests/include/BVP.hpp
@@ -8,17 +8,36 @@
 #include <petscksp.h>
 #include <vector>
 
+struct PetscShapeFunction1D {
+  Vec psi, dpsi;
+};
+
 class BVP : public TestFunction, public SetUpGrid{
     /*
     solves a two-point boundary value problem (BVP) using continuous
     or discontinuous Galerkin finite elements
     */
     public:
-    BVP(const int test_num, const bool hetero): TestFunction(test_num, hetero), SetUpGrid(){};
-    // Public member variables
-    std::vector<double> soln;
-    // Public member functions
-    PetscErrorCode CFEM_1D(int argc, char **args);
+        BVP(const int test_num, const bool hetero): TestFunction(test_num, hetero), SetUpGrid(){};
+        // Public member variables
+        std::vector<double> soln;
+        // Public member functions
+        PetscErrorCode CFEM_1D(int argc, char **args);
+    private:
+        // PetscError code
+        PetscErrorCode ierr;
+        // Global PETSc matrices (n x n)
+        Mat stiff, mass;
+        Vec rhsf;
+        // Local PETSc matrices (ord x ord)
+        Mat m,k;
+        Vec f;
+        // matrices used for generation of local petsc matrices (m & k)
+        Mat psi, dpsi, dpsiMat, psiMat;
+
+        // Private member functions
+        PetscErrorCode AssignEvaldBasis(const int ord, const double dx, const PetscShapeFunction1D &shape1d);
+        PetscErrorCode PetscEvalBasis1D(const double x, const int n, PetscShapeFunction1D &shape);
 };
 
 #endif
--- c++/PETSc_Tests/src/BVP.cpp
+++ c++/PETSc_Tests/src/BVP.cpp
@@ -5,10 +5,8 @@ PetscErrorCode BVP::CFEM_1D(int argc, char **args){
     1D CFEM solver uing PETSC ksp functionality
     */
     // PETSc initialization information
-    PetscErrorCode ierr;
-    PetscInt i, n = 10, col[3], its;
+    PetscInt n = 10;
     PetscMPIInt size;
-    PetscScalar value[3];
     ierr = PetscInitialize(&argc, &args, (char *)0, help); CHKERRQ(ierr);
     ierr = MPI_Comm_size(PETSC_COMM_WORLD,&size);CHKERRQ(ierr);
     if (size != 1) SETERRQ(PETSC_COMM_WORLD,1,"This is a uniprocessor example only!");
@@ -18,32 +16,33 @@ PetscErrorCode BVP::CFEM_1D(int argc, char **args){
     PetscInt N = info.nnodes;
     PetscInt ord = info.order[0];
     // Initialize global stiffness and mass matrices and global rhsf vector
-    Mat stiff, mass;
     ierr = MatCreate(PETSC_COMM_WORLD, &stiff); CHKERRQ(ierr);
     ierr = MatSetSizes(stiff, PETSC_DECIDE, PETSC_DECIDE, N, N); CHKERRQ(ierr);
     ierr = MatCreate(PETSC_COMM_WORLD, &mass); CHKERRQ(ierr);
     ierr = MatSetSizes(mass, PETSC_DECIDE, PETSC_DECIDE, N, N); CHKERRQ(ierr);
-    Vec rhsf;
     ierr = VecCreate(PETSC_COMM_WORLD, &rhsf); CHKERRQ(ierr);
     ierr = VecSetSizes(rhsf, PETSC_DECIDE, N); CHKERRQ(ierr);
     // Declare m, k, f local matrices/vectors
-    Mat m,k;
     ierr = MatCreate(PETSC_COMM_WORLD, &m); CHKERRQ(ierr);
     ierr = MatSetSizes(m, PETSC_DECIDE, PETSC_DECIDE, ord, ord); CHKERRQ(ierr);
     ierr = MatCreate(PETSC_COMM_WORLD, &k); CHKERRQ(ierr);
     ierr = MatSetSizes(k, PETSC_DECIDE, PETSC_DECIDE, ord, ord); CHKERRQ(ierr);
-    Vec f;
     ierr = VecCreate(PETSC_COMM_WORLD, &f); CHKERRQ(ierr);
     ierr = VecSetSizes(f, PETSC_DECIDE, ord); CHKERRQ(ierr);
     // Declare shape function matrices
-    Mat dpsiMat, psiMat;
+    ierr = MatCreate(PETSC_COMM_WORLD, &dpsi); CHKERRQ(ierr);
+    ierr = MatCreate(PETSC_COMM_WORLD, &psi); CHKERRQ(ierr);
     ierr = MatCreate(PETSC_COMM_WORLD, &dpsiMat); CHKERRQ(ierr);
+    ierr = MatCreate(PETSC_COMM_WORLD, &psiMat); CHKERRQ(ierr);
+    ierr = MatSetSizes(dpsi, PETSC_DECIDE, PETSC_DECIDE, ord, ord); CHKERRQ(ierr);
+    ierr = MatSetSizes(psi, PETSC_DECIDE, PETSC_DECIDE, ord, ord); CHKERRQ(ierr);
     ierr = MatSetSizes(dpsiMat, PETSC_DECIDE, PETSC_DECIDE, ord, ord); CHKERRQ(ierr);
+    ierr = MatSetSizes(psiMat, PETSC_DECIDE, PETSC_DECIDE, ord, ord); CHKERRQ(ierr);
 
     QuadParams1D qps1d;
     get1D_QPs(info.maxord, qps1d);
     
-    ShapeFunction1D shape1d;
+    PetscShapeFunction1D shape1d;
     
     double xL, xR, dx, x;
     double Dval, SigAbs, fval;
@@ -56,7 +55,7 @@ PetscErrorCode BVP::CFEM_1D(int argc, char **args){
             /* map from ref elem to real elem */
             x = xL + (1.0 + qps1d.xw[l1]) * dx;
             /* evaluate basis functions */
-            EvalBasis1D(qps1d.xw[l1], info.order[elem], shape1d);
+            ierr = PetscEvalBasis1D(qps1d.xw[l1], info.order[elem], shape1d); CHKERRQ(ierr);
             /* assign eval'd shape funcs to petsc matrices */
 
             /* evaluate known functions */
@@ -67,4 +66,78 @@ PetscErrorCode BVP::CFEM_1D(int argc, char **args){
     // all petsc based functions need to end with PetscFinalize()
     ierr = PetscFinalize();
     return ierr;
+}
+
+PetscErrorCode BVP::PetscEvalBasis1D(const double x, const int n, PetscShapeFunction1D &shape){
+  // shape function on reference element (-1,1)
+  // FEM order (n), has (n+1) quadrature points, and integrates (2n-1) functions EXACTLY
+  ierr = VecCreate(PETSC_COMM_WORLD, &shape.psi); CHKERRQ(ierr);
+  ierr = VecCreate(PETSC_COMM_WORLD, &shape.dpsi); CHKERRQ(ierr);
+  ierr = VecSetSizes(shape.psi, PETSC_DECIDE, n); CHKERRQ(ierr);
+  ierr = VecSetSizes(shape.dpsi, PETSC_DECIDE, n); CHKERRQ(ierr);
+  ierr = VecSetFromOptions(shape.psi); CHKERRQ(ierr);
+  ierr = VecSetFromOptions(shape.dpsi); CHKERRQ(ierr);
+  if (n==2){ //for linear FE's (n=1). Integrates up to linear FE's exactly.
+    PetscInt i0 = 0, i1 = 1;
+    PetscScalar v1,v2,v3,v4;
+    v1 = 0.5 * (1.0 - x);
+    v2 = 0.5 * (1.0 + x);
+    v3 = -0.5;
+    v4 = 0.5;
+    ierr = VecSetValues(shape.psi, 1, &i0, &v1, INSERT_VALUES); CHKERRQ(ierr);
+    ierr = VecSetValues(shape.psi, 1, &i1, &v2, INSERT_VALUES); CHKERRQ(ierr);
+    ierr = VecSetValues(shape.dpsi, 1, &i0, &v3, INSERT_VALUES); CHKERRQ(ierr);
+    ierr = VecSetValues(shape.dpsi, 1, &i1, &v4, INSERT_VALUES); CHKERRQ(ierr);
+  }
+  else if (n==3){ //for quadratic FE's (n=2). Integrates up to cubic FE's exactly.
+    PetscInt i0 = 0, i1 = 1, i2 = 2;
+    PetscScalar v1, v2, v3, v4, v5, v6;
+    v1 = (pow(x,2)-x)/2.0;
+    v2 = 1-pow(x,2);
+    v3 = (pow(x,2)+x)/2.0;
+    v4 = x-1./2.;
+    v5 = -2.0*x;
+    v6 = x+1./2.;
+    ierr = VecSetValues(shape.psi, 1, &i0, &v1, INSERT_VALUES); CHKERRQ(ierr);
+    ierr = VecSetValues(shape.psi, 1, &i1, &v2, INSERT_VALUES); CHKERRQ(ierr);
+    ierr = VecSetValues(shape.psi, 1, &i2, &v3, INSERT_VALUES); CHKERRQ(ierr);
+    ierr = VecSetValues(shape.dpsi, 1, &i0, &v4, INSERT_VALUES); CHKERRQ(ierr);
+    ierr = VecSetValues(shape.dpsi, 1, &i1, &v5, INSERT_VALUES); CHKERRQ(ierr);
+    ierr = VecSetValues(shape.dpsi, 1, &i2, &v6, INSERT_VALUES); CHKERRQ(ierr);
+  }
+  else if (n==4){ //for cubic FE's (n=3). Integrates up to 5th order FE's exactly.
+    PetscInt i0 = 0, i1 = 1, i2 = 2, i3 = 2;
+    PetscScalar v1, v2, v3, v4, v5, v6, v7, v8;
+    v1 = 1./16.*(-9.*pow(x,3) + 9.*pow(x,2) + x - 1.);
+    v2 = 1./16.*(27.*pow(x,3) - 9.*pow(x,2) - 27.*x + 9.);
+    v3 = 1./16.*(-27.*pow(x,3) - 9.*pow(x,2) + 27.*x + 9.);
+    v4 = 1./16.*(9.*pow(x,3) + 9.*pow(x,2) - x - 1.);
+    v5 =  1./16.*(-27.*pow(x,2) + 18.*x + 1.);
+    v6 =  1./16.*(81.*pow(x,2) - 18.*x - 27.);
+    v7 =  1./16.*(-81.*pow(x,2) - 18.*x + 27.);
+    v8 =  1./16.*(27.*pow(x,2) + 18.*x - 1.);
+    ierr = VecSetValues(shape.psi, 1, &i0, &v1, INSERT_VALUES); CHKERRQ(ierr);
+    ierr = VecSetValues(shape.psi, 1, &i1, &v2, INSERT_VALUES); CHKERRQ(ierr);
+    ierr = VecSetValues(shape.psi, 1, &i2, &v3, INSERT_VALUES); CHKERRQ(ierr);
+    ierr = VecSetValues(shape.psi, 1, &i3, &v4, INSERT_VALUES); CHKERRQ(ierr);
+    ierr = VecSetValues(shape.dpsi, 1, &i0, &v5, INSERT_VALUES); CHKERRQ(ierr);
+    ierr = VecSetValues(shape.dpsi, 1, &i1, &v6, INSERT_VALUES); CHKERRQ(ierr);
+    ierr = VecSetValues(shape.dpsi, 1, &i2, &v7, INSERT_VALUES); CHKERRQ(ierr);
+    ierr = VecSetValues(shape.dpsi, 1, &i3, &v8, INSERT_VALUES); CHKERRQ(ierr);
+  }
+  else{
+    printf("Order = %d shape function does not exist.",n);
+    exit(1);
+  }
+  // Assemble the vectors
+  VecAssemblyBegin(shape.psi);
+  VecAssemblyBegin(shape.dpsi);
+  VecAssemblyEnd(shape.psi);
+  VecAssemblyEnd(shape.dpsi);
+  
+  // view the vectors
+  VecView(shape.psi, PETSC_VIEWER_STDOUT_WORLD);  // petsc_viewer_stdout_world allows for
+  VecView(shape.dpsi, PETSC_VIEWER_STDOUT_WORLD); // sequential and parallel vectors
+
+  return ierr;
 }
