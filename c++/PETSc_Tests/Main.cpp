63de96e294e16f312c08010f75a43cea822874ce
albeanth <albertia@oregonstate.edu>
2019-12-09 16:45:26 -0800

    updating Main.cpp to include nonlinear tests

--- c++/PETSc_Tests/Main.cpp
+++ c++/PETSc_Tests/Main.cpp
@@ -1,21 +1,33 @@
 #include "BVP.hpp"
+#include "Nonlinear.hpp"
 #include <iostream>
 #include <vector>
 
 int main(int argc,char **args){
     const std::vector<int> NumOfElem {64};
-    const int selection {1};
+    /*
+     * test #'s < 100
+     *   CFEM using petsc ksp libary tests
+     * test #'s \geq 100
+     *   nonlinear solver using petsc snes library tests
+     */
+    const int selection {100};
     const bool hetero {false};
     const std::vector<double> Bnds {0.0, 1.0};
 
-    std::vector<double> L2Error (NumOfElem.size(), 0.0);
-    std::vector<double> H1Error (NumOfElem.size(), 0.0);
-    BVP CFEM(selection, hetero);
-    for (int i=0; i<NumOfElem.size(); i++){
-        CFEM.add_CFEMGrid(NumOfElem[i], 1, Bnds);
-        CFEM.CFEM_1D(argc, args);
+    if (selection < 100){
+        std::vector<double> L2Error (NumOfElem.size(), 0.0);
+        std::vector<double> H1Error (NumOfElem.size(), 0.0);
+        BVP CFEM(selection, hetero);
+        for (int i=0; i<NumOfElem.size(); i++){
+            CFEM.add_CFEMGrid(NumOfElem[i], 1, Bnds);
+            CFEM.CFEM_1D(argc, args);
+        }
     }
-
+    else{
+        NonLinear NLTest(selection, hetero);
+        NLTest.NL_1D(argc, args);
+    }
 
     return 0;
 }
