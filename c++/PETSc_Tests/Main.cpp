678f72ea9ae22cd0b0231687699e3dc1d6571716
albeanth <albertia@oregonstate.edu>
2020-01-21 12:21:15 -0800

    adding in commented out code to print l2/h1 error data as a fucntion of h. - is commented out because it will require a movement of petsc into the main file and it was a pain to get going right away. - updating this is put on the backburner a little bit

--- c++/PETSc_Tests/Main.cpp
+++ c++/PETSc_Tests/Main.cpp
@@ -11,13 +11,18 @@ int main(int argc,char **args){
      * test #'s \geq 100
      *   nonlinear solver using petsc snes library tests
      */
-    const int selection {100};
+    const int selection {101};
     const bool hetero {false};
     const std::vector<double> Bnds {0.0, 1.0};
 
+    /* vectors to store errors */
+    // std::vector<double> h;
+    // std::vector<double> L2Error_Vel;
+    // std::vector<double> L2Error_Rho;
+    // std::vector<double> H1Error_Vel;
+    // std::vector<double> H1Error_Rho;
+
     if (selection < 100){
-        std::vector<double> L2Error (NumOfElem.size(), 0.0);
-        std::vector<double> H1Error (NumOfElem.size(), 0.0);
         BVP CFEM(selection, hetero, Bnds);
         for (int i=0; i<NumOfElem.size(); i++){
             CFEM.add_CFEMGrid(NumOfElem[i], 1);
@@ -29,8 +34,18 @@ int main(int argc,char **args){
         for (int i = 0; i < NumOfElem.size(); i++) {
             NLTest.add_DFEMGrid(NumOfElem[i], 1);
             NLTest.NL_1D(argc, args);
+            // L2Error_Vel[i] = NLTest.l2Err_Vel;
+            // L2Error_Rho[i] = NLTest.l2Err_Rho;
+            // H1Error_Vel[i] = NLTest.h1Err_Vel;
+            // H1Error_Rho[i] = NLTest.h1Err_Rho;
+            // h[i] = NLTest.info.hel;
         }
     }
+
+    // printf("h\tL2Error");
+    // for (int i=0; i<NumOfElem.size(); i++){
+    //     printf("%.4e\t%.8e\t%.8e\n", h[i], L2Error_Vel[i], L2Error_Rho[i]);
+    // }
 
     return 0;
 }
